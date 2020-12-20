3e442bdd2402acbea5ec7f99e0bed7e0ee0b1b21
sonic757 <45443026+sonic757@users.noreply.github.com>
2020-12-20 15:04:51 -0800

    Update cumbot.py

--- cumbot/cumbot.py
+++ cumbot/cumbot.py
@@ -6,14 +6,14 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='$POG')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command()
-async def fard(ctx, arg):
+async def cum(ctx, arg):
     if(!isinstance(arg, int))
         await ctx.send(arg + "is not an integer, try again")
     else
