0079299094c934c01a4faefa609ff84aecc3dfb8
sonic757 <45443026+sonic757@users.noreply.github.com>
2020-12-20 14:54:10 -0800

    Update cumbot.py

--- cumbot/cumbot.py
+++ cumbot/cumbot.py
@@ -8,30 +8,12 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
-
-#Game Variables
-gunChambers = 6
-
-
-
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.event
-async def on_message(message): 
-    if message.content.startswith("!gb marco"): 
-        channel = message.channel
-        await channel.send("POLO!")    
-
-    if message.content.startswith("!gb RR new"):
-        channel = message.channel
-        await channel.send("New Game Russian Roulette")
-        gunChambers = 6
-        
-    #if message.content.startswith("!gb RR fire"):
-
-
-
+@bot.command
+async def fard(ctx, arg):
+    await ctx.send(arg)
 
 bot.run(TOKEN)
